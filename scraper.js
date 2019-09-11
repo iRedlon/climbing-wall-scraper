9cb07377df645d5fa0580673926ed105656e7b5e
Isaac Redlon <isaacredlon@gmail.com>
2019-09-11 13:50:19 -0400

    Cleaned up code style

--- scraper.js
+++ scraper.js
@@ -1,26 +1,29 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
 
-const url = 'https://scripts.mit.edu/~mitoc/wall/';
+// imports
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-const control_text = 'There are no hours posted at this time, check back soon!';
+
+// constants
+const url = "https://scripts.mit.edu/~mitoc/wall/";
+const control_text = "There are no hours posted at this time, check back soon!";
 
 
 axios.get(url)
-    .then(response => {
-    console.log(getData(response.data));
-})
-    .catch(error => {
-    console.log(error);
-})
-
-function handle_response(html) {
-    const $ = cheerio.load(html);
-    let text = "";
-    $('table.timeline').each((i, elem) => {
-        text = $(elem).text()
+    .then(res => {
+        console.log(handleRes(res));
     })
-    if(control_text !== text){
-        send_matt_message.now()
+    .catch(err => {
+        console.log(err);
+    });
+
+
+function handleRes(res) {
+    const $ = cheerio.load(res.data);
+
+    let text = $("table.timeline").text();
+
+    if (control_text !== text) {
+        // TODO: Add FB message
     }
 }
--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 
 // imports
 const express = require("express");
-const scraper = require("./scraper.js")
+const scraper = require("./scraper.js");
 
 
 // constants
 const port = process.env.PORT || 5000;
 const app = express();
-
-//gang gang
-
-
-// config
-app.use(express.static(__dirname + "/public"));
 
 
 app.get("/", function(req, res) {

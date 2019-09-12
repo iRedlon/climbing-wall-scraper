c4df1effbc1e49df7919ffe8f5ee7f357f705447
Matthew Morningstar <matt_m@mit.edu>
2019-09-12 17:55:02 -0400

    Implemented FB messenger functionality. Sent "YO MOM FAT".

---
+++ config/fbconfig.js
@@ -0,0 +1,10 @@
+const password = "climbclimbwallwall"
+const email = "matt.morningstar9@gmail.com"
+
+
+module.exports={
+    password: password,
+    email: email
+}
+
+
--- scraper.js
+++ scraper.js
@@ -2,12 +2,14 @@
 // imports
 const axios = require("axios");
 const cheerio = require("cheerio");
-
+const login = require("facebook-chat-api")
 
 // constants
 const url = "https://scripts.mit.edu/~mitoc/wall/";
 const control_text = "There are no hours posted at this time, check back soon!";
 
+const fb_email = require("./config/fbconfig").email;
+const fb_password = require("./config/fbconfig").password;
 
 axios.get(url)
     .then(res => {
@@ -17,13 +19,17 @@ axios.get(url)
         console.log(err);
     });
 
-
 function handleRes(res) {
     const $ = cheerio.load(res.data);
 
     let text = $("table.timeline").text();
 
-    if (control_text !== text) {
-        // TODO: Add FB message
-    }
+    // Create simple echo bot
+    login({email: fb_email, password: fb_password}, (err, api) => {
+        if(err) return console.error(err);
+
+        api.sendMessage("I AM OPEN.", "100000230284341")
+
+    });
+
 }
